9ef083ac6300d75f9170c3fa323360b6c508cb3c
Richard Felkl <rfelkl@mirantis.com>
2017-10-20 15:51:13 +0200

    added mirror config

---
+++ mirror_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+
+export SALT_MASTER_DEPLOY_IP=10.1.0.14
+export APTLY_DEPLOY_IP=10.1.0.14
+export APTLY_DEPLOY_NETMASK=255.255.0.0
+export APTLY_MINION_ID=apt01.deploy-name.local
+
+echo "Configuring network interfaces"
+envsubst < /root/interfaces > /etc/network/interfaces
+ifdown ens3; ifup ens3
+
+echo "Configuring salt"
+#service salt-master restart
+envsubst < /root/minion.conf > /etc/salt/minion.d/minion.conf
+service salt-minion restart
